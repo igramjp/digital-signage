fb8299d310e7622f9d845430c6f0fedfffa5d657
Manabu Igarashi <igram@igram-Mac-Studio.local>
2025-07-25 16:57:45 +0900

    update

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import "../app.css";
+  import { getCurrentTime, SIGNAGE_VERSION } from "$lib";
+
+  let now = getCurrentTime();
+</script>
+
+<h1>Hello, Digital Signage!</h1>
+<p>This is a static site powered by SvelteKit.</p>
+
+<p>現在時刻: {now}</p>
+<p>バージョン: {SIGNAGE_VERSION}</p>
